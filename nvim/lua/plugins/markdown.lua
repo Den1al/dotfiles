ea571b7b90a356af444d907198ad94e8129023ee
Daniel Abeles <dabeles@gitlab.com>
2024-07-23 12:07:08 +0300

    changed the markdown UI plugin

--- nvim/lua/plugins/markdown.lua
+++ nvim/lua/plugins/markdown.lua
@@ -1,15 +1,25 @@
 return {
-	{
-		"ellisonleao/glow.nvim",
-		config = function()
-			require("glow").setup()
-			vim.keymap.set("n", "<C-g>", ":Glow <CR>")
-			vim.api.nvim_set_hl(0, "@text.code", { bg = "#000FFF" })
-		end,
-	},
-	-- {
-	--   "lukas-reineke/headlines.nvim",
-	--   dependencies = "nvim-treesitter/nvim-treesitter",
-	--   config = true, -- or `opts = {}`
-	-- },
+  {
+    "MeanderingProgrammer/markdown.nvim",
+    main = "render-markdown",
+    opts = {},
+    name = "render-markdown",                                                    -- Only needed if you have another plugin named markdown.nvim
+    dependencies = { "nvim-treesitter/nvim-treesitter", "echasnovski/mini.nvim" }, -- if you use the mini.nvim suite
+    -- dependencies = { 'nvim-treesitter/nvim-treesitter', 'echasnovski/mini.icons' }, -- if you use standalone mini plugins
+    -- dependencies = { 'nvim-treesitter/nvim-treesitter', 'nvim-tree/nvim-web-devicons' }, -- if you prefer nvim-web-devicons
+  },
+  -- {
+  --
+  -- 	"ellisonleao/glow.nvim",
+  -- 	config = function()
+  -- 		require("glow").setup()
+  -- 		vim.keymap.set("n", "<C-g>", ":Glow <CR>")
+  -- 		vim.api.nvim_set_hl(0, "@text.code", { bg = "#000FFF" })
+  -- 	end,
+  -- },
+  -- {
+  --   "lukas-reineke/headlines.nvim",
+  --   dependencies = "nvim-treesitter/nvim-treesitter",
+  --   config = true, -- or `opts = {}`
+  -- },
 }
