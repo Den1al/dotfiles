55527830fb299c5e939db499af3822b33a3b061c
Daniel Abeles <dabeles@gitlab.com>
2024-07-24 12:08:35 +0300

    updated keys.md

--- nvim/lua/plugins/lsp-config.lua
+++ nvim/lua/plugins/lsp-config.lua
@@ -1,77 +1,97 @@
 return {
-	{
-		"williamboman/mason.nvim",
-		config = function()
-			require("mason").setup()
-		end,
-	},
-	{
-		"williamboman/mason-lspconfig.nvim",
-		config = function()
-			require("mason-lspconfig").setup({
-				ensure_installed = {
-					"lua_ls",
-					"tsserver",
-					"gopls",
-					"golangci_lint_ls",
-					"ocamllsp",
-					"marksman",
-					"pylsp",
-					"bashls",
-				},
-			})
-		end,
-	},
-	{
-		"neovim/nvim-lspconfig",
-		config = function()
-			local lspconfig = require("lspconfig")
-			local util = require("lspconfig/util")
+  {
+    "williamboman/mason.nvim",
+    config = function()
+      require("mason").setup()
+    end,
+  },
+  {
+    "williamboman/mason-lspconfig.nvim",
+    config = function()
+      require("mason-lspconfig").setup({
+        ensure_installed = {
+          "lua_ls",
+          "tsserver",
+          "gopls",
+          "golangci_lint_ls",
+          "ocamllsp",
+          "marksman",
+          "pylsp",
+          "bashls",
+        },
+      })
+    end,
+  },
+  {
+    "neovim/nvim-lspconfig",
+    config = function()
+      local lspconfig = require("lspconfig")
+      local util = require("lspconfig/util")
 
-			lspconfig.lua_ls.setup({
-				settings = {
-					Lua = {
-						diagnostics = {
-							globals = { "vim" },
-						},
-					},
-				},
-			})
-			lspconfig.pylsp.setup({})
-			lspconfig.marksman.setup({})
-			lspconfig.tsserver.setup({})
-			lspconfig.bashls.setup({})
-			lspconfig.ocamllsp.setup({
-				cmd = { "ocamllsp" },
-				filetypes = { "ocaml", "ocaml.menhir", "ocaml.interface", "ocaml.ocamllex", "reason", "dune" },
-				root_dir = util.root_pattern(
-					"*.opam",
-					"esy.json",
-					"package.json",
-					".git",
-					"dune-project",
-					"dune-workspace"
-				),
-			})
+      lspconfig.lua_ls.setup({
+        settings = {
+          Lua = {
+            diagnostics = {
+              globals = { "vim" },
+            },
+          },
+        },
+      })
+      lspconfig.pylsp.setup({})
+      lspconfig.marksman.setup({})
+      lspconfig.tsserver.setup({})
+      lspconfig.bashls.setup({})
+      lspconfig.ocamllsp.setup({
+        cmd = { "ocamllsp" },
+        filetypes = { "ocaml", "ocaml.menhir", "ocaml.interface", "ocaml.ocamllex", "reason", "dune" },
+        root_dir = util.root_pattern(
+          "*.opam",
+          "esy.json",
+          "package.json",
+          ".git",
+          "dune-project",
+          "dune-workspace"
+        ),
+      })
 
-			lspconfig.gopls.setup({
-				cmd = { "gopls" },
-				filetypes = { "go", "gomod", "gowork", "gotmpl" },
-				root_dir = util.root_pattern("go.work", "go.mod", ".git"),
-				settings = {
-					gopls = {
-						completeUnimported = true,
-						usePlaceholders = true,
-						analyses = {
-							unusedparams = true,
-						},
-					},
-				},
-			})
+      lspconfig.gopls.setup({
+        cmd = { "gopls" },
+        filetypes = { "go", "gomod", "gowork", "gotmpl" },
+        root_dir = util.root_pattern("go.work", "go.mod", ".git"),
+        settings = {
+          gopls = {
+            completeUnimported = true,
+            usePlaceholders = true,
+            analyses = {
+              unusedparams = true,
+            },
+          },
+        },
+      })
 
-			vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
-			vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
-			vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, {})
-		end,
-	},
+      local nmap = function(keys, func, desc)
+        if desc then
+          desc = "LSP: " .. desc
+        end
+
+        vim.keymap.set("n", keys, func, { desc = desc })
+      end
+      -- key bindings
+      nmap("<leader>rn", vim.lsp.buf.rename, "[R]e[n]ame")
+      nmap("<leader>ca", vim.lsp.buf.code_action, "[C]ode [A]ction")
+      nmap("gd", vim.lsp.buf.definition, "[G]oto [D]efinition")
+      nmap("gr", require("telescope.builtin").lsp_references, "[G]oto [R]eferences")
+      nmap("gI", vim.lsp.buf.implementation, "[G]oto [I]mplementation")
+      nmap("<leader>D", vim.lsp.buf.type_definition, "Type [D]efinition")
+      nmap("<leader>ds", require("telescope.builtin").lsp_document_symbols, "[D]ocument [S]ymbols")
+      nmap("<leader>ws", require("telescope.builtin").lsp_dynamic_workspace_symbols, "[W]orkspace [S]ymbols")
+
+      -- See `:help K` for why this keymap
+      nmap("K", vim.lsp.buf.hover, "Hover documentation")
+      -- nmap("K", vim.lsp.buf.signature_help, "Signature Documentation")
+
+      -- Lesser used LSP functionality
+      nmap("gD", vim.lsp.buf.declaration, "[G]oto [D]eclaration")
+    end,
+  },
 }
