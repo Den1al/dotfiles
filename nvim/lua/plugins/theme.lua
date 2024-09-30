6082a2eda24d1f7636684b6de08183d3d14ff131
Daniel Abeles <dabeles@gitlab.com>
2024-09-30 09:07:28 +0300

    nvim now uses tokyonight

---
+++ nvim/lua/plugins/theme.lua
@@ -0,0 +1,24 @@
+-- return {
+--   "catppuccin/nvim",
+--   name = "catppuccin",
+--   priority = 1000,
+--   config = function()
+--     require("catppuccin").setup({
+--       flavour = "frappe",
+--       transparent_background = false,
+--     })
+--     vim.cmd.colorscheme("catppuccin")
+--   end,
+-- }
+--
+--
+return {
+	"folke/tokyonight.nvim",
+	opts = {
+		transparent = true,
+		styles = {
+			sidebars = "transparent",
+			floats = "transparent",
+		},
+	},
+}
