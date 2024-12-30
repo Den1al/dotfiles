5a87043cb0d6c0152426a6fa14d462c027051ae1
Daniel Abeles <dabeles@gitlab.com>
2024-12-30 11:05:23 +0200

    added dap support

--- nvim/lua/plugins/kitty.lua
+++ nvim/lua/plugins/kitty.lua
@@ -7,6 +7,33 @@ return {
 	-- version = '*', -- latest stable version, may have breaking changes if major version changed
 	-- version = '^5.0.0', -- pin major version, include fixes and features that do not have breaking changes
 	config = function()
-		require("kitty-scrollback").setup()
+		require("kitty-scrollback").setup({
+			-- global configuration
+			{
+				status_window = {
+					autoclose = true,
+				},
+			},
+			-- builtin configuration override
+			ksb_builtin_get_text_all = {
+				kitty_get_text = {
+					ansi = true,
+				},
+			},
+			-- user defined configuration table
+			myconfig = {
+				kitty_get_text = {
+					ansi = true,
+				},
+			},
+			-- user defined configuration function
+			myfnconfig = function(kitty_data)
+				return {
+					kitty_get_text = {
+						extent = "all",
+					},
+				}
+			end,
+		})
 	end,
 }
