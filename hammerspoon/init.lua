c1c34bb840e192df01602053c072ba0b99a2fe81
Daniel Abeles <abeles22@gmail.com>
2026-02-17 13:35:27 +0200

    revert to file based selection

--- hammerspoon/init.lua
+++ hammerspoon/init.lua
@@ -28,7 +28,11 @@ end)
 
 hs.loadSpoon("ClaudeRewriter")
 
-spoon.ClaudeRewriter.apiKey = os.getenv("ANTHROPIC_API_KEY")
+local f = io.open(os.getenv("HOME") .. "/.config/gitlab-duo/a-key", "r")
+if f then
+	spoon.ClaudeRewriter.apiKey = f:read("*l")
+	f:close()
+end
 
 spoon.ClaudeRewriter:bindHotkeys({
 	rewrite = { { "alt" }, "c" },
